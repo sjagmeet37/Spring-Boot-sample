c7e72fbd58cb73f81a617a3c49ba1802bbee4cb0
sjagmeet37 <jagmeet.singh03@sap.com>
2019-02-25 21:06:47 +0530

    second commit

--- src/main/java/com/sap/singleaccess/DbSpringApplication.java
+++ src/main/java/com/sap/singleaccess/DbSpringApplication.java
@@ -2,10 +2,25 @@ package com.sap.singleaccess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class DbSpringApplication {
 
+@Configuration
+@ComponentScan
+
+@SpringBootApplication(
+	       exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+public class DbSpringApplication extends SpringBootServletInitializer{
+
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	    {
+	        return application.sources(DbSpringApplication.class);
+	    }
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(DbSpringApplication.class, args);
 	}
